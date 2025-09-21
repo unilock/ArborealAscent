deb5218b2540a6aa4e304e4e57aa39d02d2205bb
unilock <katherines1337@outlook.com>
2025-09-21 09:26:06 -0400

    only allow climbing upright logs

---
+++ xplat/src/main/java/cc/unilock/treeclimbing/mixin/BlockBehaviourMixin.java
@@ -0,0 +1,28 @@
+package cc.unilock.treeclimbing.mixin;
+
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import net.minecraft.core.Direction;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.world.level.block.RotatedPillarBlock;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.AABB;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(BlockBehaviour.class)
+public class BlockBehaviourMixin {
+	@Unique
+	private static final VoxelShape LOG = Shapes.create(new AABB(0, 0, 0, 1, 1, 1).contract(0.03125, 0, 0.03125));
+
+	@ModifyReturnValue(method = "getCollisionShape(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/world/level/BlockGetter;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/phys/shapes/CollisionContext;)Lnet/minecraft/world/phys/shapes/VoxelShape;", at = @At("RETURN"))
+	private VoxelShape getCollisionShape(VoxelShape original, BlockState state) {
+		if (state.is(BlockTags.LOGS) && state.hasProperty(RotatedPillarBlock.AXIS) && Direction.Axis.Y.equals(state.getValue(RotatedPillarBlock.AXIS))) {
+			return LOG;
+		}
+		return original;
+	}
+}
